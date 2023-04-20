4405832e65ef3f02c5a98a1da04b18bcf0f18076
Mamaklicengo <128487934+Mamaklicengo@users.noreply.github.com>
2023-04-20 19:14:33 +0300

    Update responses.py

--- responses.py
+++ responses.py
@@ -4,23 +4,7 @@ def Bot_Response(message, response_array, response):
 
     # Splits the message and the punctuation into an array
 
-    list_message = re.findall(r"[\w']+|[.,!?;]", message.lower())
-
-    # Scores the amount of words in the message
-
-    score = 0
-
-    for word in list_message:
-
-        if word in response_array:
-
-            score = score + 1
-
-    # Returns the response and the score of the response
-
-    # print(score, response)
-
-    return [score, response]
+    
 
 def get_response(message):
 
@@ -110,26 +94,7 @@ def get_response(message):
 
     # Checks all of the response scores and returns the best matching response
 
-    response_scores = []
-
-    for response in response_list:
-
-        response_scores.append(response[0])
-
-    # Get the max value for the best response and store it into a variable
-
-    winning_response = max(response_scores)
-
-    matching_response = response_list[response_scores.index(winning_response)]
-
-    # Return the matching response to the user
-       if winning_response == 0:
-
-        bot_response = ''
-
-    else:
-
-        bot_response = matching_response[1]
+]
    
 
     print('Bot response:', bot_response)
