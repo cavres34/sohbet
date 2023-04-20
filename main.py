17b149a708a9ca2ea236444febc2f703bbfdbc7d
Mamaklicengo <128487934+Mamaklicengo@users.noreply.github.com>
2023-04-20 18:14:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 
 import logging
 
-from scraper import devtoTop, devtoLatest, get_tldr, get_medium, get_quote, get_techcrunch, get_hackerNews
+from scraper import get_tldr, get_medium, get_quote, get_techcrunch, get_hackerNews
 
 from keep_alive import keep_alive
 
