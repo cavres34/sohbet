704fa196e5e7f9b7d32c048613856d405f0ee676
Mamaklicengo <128487934+Mamaklicengo@users.noreply.github.com>
2023-04-21 21:46:36 +0300

    Update responses.py

--- responses.py
+++ responses.py
@@ -19,7 +19,7 @@ def Bot_Response(message, response_array, response):
 def get_response(message):
     # Add your custom responses here
     response_list = [
-        Bot_Response(message, ['selam', 'merba', 'hello', 'iyi sen nasılsın ')
+        Bot_Response(message, ['selam', 'merba', 'hello', 'iyi sen nasılsın '),
 İ'],
                      'Hello there, your AI companion is here to serve you.\nYou can talk normally or Type (cmd) to get started '),
 
