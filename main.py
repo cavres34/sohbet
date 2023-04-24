dbf4263f87b738f02b9aa8ff3c7729f0ec0cacf0
Mamaklicengo <128487934+Mamaklicengo@users.noreply.github.com>
2023-04-24 13:49:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def start(update, context):
 
     update.message.reply_text(
 
-        "Merhaba ben geveze 😂 Grup içinde sizinle sohbet ederim❤️ Beni grubuna al sizde sohbet edeyim😝")
+        "Merhaba ben geveze 😂 \n Grup içinde sizinle sohbet ederim❤️ \n Beni grubuna al sizde sohbet edeyim😝")
          
 
 def cmd(update, context):
