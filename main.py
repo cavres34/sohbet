4929bf21d7465b3cb29d7191ac95fc0993e0aaed
yildizcavres <129795323+yildizcavres@users.noreply.github.com>
2023-04-29 13:59:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def start(update, context):
 
     update.message.reply_text(
 
-        "Merhaba ben sizinle konuşan 😂 \n Grup içinde sizinle sohbet ederim❤️ \n Beni gruba al sizlede sohbet edeyim😝")
+        "Merhaba ben çavreş tarafından sizinle sohbet etmek için geliştirildi 😂 \n Grup içinde sizinle sohbet ederim❤️ \n Beni gruba al sizlede sohbet edeyim😝")
          
 
 def cmd(update, context):
